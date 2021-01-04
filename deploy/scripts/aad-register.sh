d28e229c5ab34c66a47ecd882d99c7421c491f04
Marc Schier <mschier@microsoft.com>
2021-01-04 15:58:35 +0100

    update

--- deploy/scripts/aad-register.sh
+++ deploy/scripts/aad-register.sh
@@ -9,13 +9,10 @@ usage(){
 register_service(){
     if [[ -z "$applicationName" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
     echo "Registering application '$applicationName' in AAD tenant..."
-    objectId=$(az ad app create --display-name $applicationName --homepage "https://localhost" --query objectId -o tsv)
-    objectId=${objectId//[[:space:]]}
+    objectId=$(az ad app create --display-name $applicationName --homepage "https://localhost" --query objectId -o tsv | tr -d '\r')
     pw=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
-    tenantId=$(az ad app credential reset --id $objectId --append --password $pw --query tenant -o tsv)
-    tenantId=${tenantId//[[:space:]]}
-    tenantName=$(az ad app show --id $objectId --query publisherDomain -o tsv)
-    tenantName=${tenantName//[[:space:]]}
+    tenantId=$(az ad app credential reset --id $objectId --append --password $pw --query tenant -o tsv | tr -d '\r')
+    tenantName=$(az ad app show --id $objectId --query publisherDomain -o tsv | tr -d '\r')
     az ad app update --id $objectId --identifier-uris https://$tenantName/$applicationName \
         --set publicClient=false \
         --set knownClientApplications='["04b07795-8ddb-461a-bbee-02f9e1bf7b46", "872cd9fa-d31f-45e0-9eab-6e460a02d1f1"]'
@@ -28,11 +25,13 @@ register_service(){
 unregister_service(){
     if [[ -z "$applicationName" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
     echo "Unregistering application '$applicationName' from AAD tenant..."
-    for id in $(az ad app list --display-name $applicationName --query objectId -o tsv); do
-        objectId=${id//[[:space:]]}
+    for objectId in $(az ad app list --display-name $applicationName --query [].objectId -o tsv | tr -d '\r'); do
         az ad app delete --id $objectId
+        echo "Application '$objectId' unregistered from AAD tenant..."
     done
 }
+
+if [[ -n "$AZ_SCRIPTS_OUTPUT_PATH" ]] ; then set -x; az login --identity; fi
 
 applicationName=
 
@@ -46,7 +45,6 @@ while [ "$#" -gt 0 ]; do
     esac
     shift
 done
-if [[ -n "$AZ_SCRIPTS_OUTPUT_PATH" ]] ; then az login --identity; fi
 [ "$c" == "register-service" ] && { register_service; exit 0; }
 [ "$c" == "unregister-service" ] && { unregister_service; exit 0; }
 usage
