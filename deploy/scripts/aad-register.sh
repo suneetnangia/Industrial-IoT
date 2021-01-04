528d3247c9829d30d74fe5c6997fb3996f30c88a
Marc Schier <mschier@microsoft.com>
2021-01-04 20:43:37 +0100

    update

--- deploy/scripts/aad-register.sh
+++ deploy/scripts/aad-register.sh
@@ -1,38 +1,38 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 usage(){
     echo "Usage: $0 register-service, unregister-service, register-client, unregister-client with with --name applicationname"
     exit 1
 }
 
-# Register service application in aad
-register_service(){
-    if [[ -z "$applicationName" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
-    echo "Registering application '$applicationName' in AAD tenant..."
-    objectId=$(az ad app create --display-name $applicationName --homepage "https://localhost" --query objectId -o tsv | tr -d '\r')
-    pw=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
-    tenantName=$(az ad app show --id $objectId --query publisherDomain -o tsv | tr -d '\r')
-    az ad app update --id $objectId --identifier-uris https://$tenantName/$applicationName \
-        --set publicClient=false \
-        --set knownClientApplications='["04b07795-8ddb-461a-bbee-02f9e1bf7b46", "872cd9fa-d31f-45e0-9eab-6e460a02d1f1"]'
-    echo "Application '$objectId' registered in AAD tenant."
-    az ad app credential reset --id $objectId --append --password $pw -o none
-    az ad app show --id $objectId \
-        --query "{serviceAppId:appId, servicePrincipalId:objectId, serviceAppSecret:'$pw', serviceAudience:identifierUris[0]}" -o json \
-        | tee $AZ_SCRIPTS_OUTPUT_PATH
-}
-
 # Unregister service application in aad
 unregister_service(){
-    if [[ -z "$applicationName" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
-    echo "Unregistering application '$applicationName' from AAD tenant..."
+    if [[ -z "$1" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
+    echo "Unregistering application '$1' from AAD tenant..."
     for objectId in $(az ad app list --display-name $applicationName --query [].objectId -o tsv | tr -d '\r'); do
         az ad app delete --id $objectId
         echo "Application '$objectId' unregistered from AAD tenant."
     done
 }
 
-if [[ -n "$AZ_SCRIPTS_OUTPUT_PATH" ]] ; then set -x; az login --identity; fi
+# Register service application in aad using application developer role permissions (can create, not update)
+register_service(){
+    if [[ -z "$1" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
+    echo "Registering application '$1' in AAD tenant..."
+    pw=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
+    tenantName=$(az ad app list --all --query [0].publisherDomain -o tsv | tr -d '\r')
+    if [[ -z "$tenantName" ]] ; then
+        tenantName=$(az ad app create --display-name "__delete_me_$1" --query publisherDomain -o tsv | tr -d '\r')
+    fi
+    objectId=$(az ad app create --display-name $1 --password $pw --identifier-uris https://$tenantName/$1 --homepage "https://localhost" --query objectId -o tsv | tr -d '\r')
+    az ad app update --id $objectId --set publicClient=false 
+    az ad app update --id $objectId --set knownClientApplications='["04b07795-8ddb-461a-bbee-02f9e1bf7b46", "872cd9fa-d31f-45e0-9eab-6e460a02d1f1"]'
+    # az ad app credential reset --id $objectId --append --password $pw -o none
+    echo "Application '$objectId' registered in AAD tenant."
+    az ad app show --id $objectId --query "{serviceAppId:appId, servicePrincipalId:objectId, serviceAppSecret:'$pw', serviceAudience:identifierUris[0]}" -o json | tee $AZ_SCRIPTS_OUTPUT_PATH
+}
+
+if [[ -n "$AZ_SCRIPTS_OUTPUT_PATH" ]] ; then set -ex; az login --identity; fi
 
 applicationName=
 
@@ -46,6 +46,6 @@ while [ "$#" -gt 0 ]; do
     esac
     shift
 done
-[ "$c" == "register-service" ] && { register_service; exit 0; }
-[ "$c" == "unregister-service" ] && { unregister_service; exit 0; }
+[ "$c" == "register-service" ] && { register_service $applicationName; exit 0; }
+[ "$c" == "unregister-service" ] && { unregister_service $applicationName; exit 0; }
 usage
