43eb97e9def1a62a8c44add0bf5776795b8a9bb5
Marc Schier <mschier@microsoft.com>
2021-01-05 10:46:15 +0100

    update

--- deploy/scripts/aad-assign.sh
+++ deploy/scripts/aad-assign.sh
@@ -1,31 +1,51 @@
 #!/bin/bash -ex
 
-roleTemplateId=cf1c38e5-3621-4004-a7cb-879624dced7c # Application developer role
-# roleTemplateId=158c047a-c907-4556-b7ef-446551a6b5f7 # Cloud application admin role
-name=
-resourcegroup=
+name=aad5
+resourcegroup=testgroup1
 
 while [ "$#" -gt 0 ]; do
     case "$1" in
         --name)                  name="$2" ;;
-        --roleTemplateId)        roleTemplateId="$2" ;;
         --resourcegroup)         resourcegroup="$2" ;;
     esac
     shift
 done
 if [[ -z "$resourcegroup" ]]; then echo "Parameter is empty or missing: --resourcegroup"; exit 1; fi
 if [[ -z "$name" ]]; then echo "Parameter is empty or missing: --name"; exit 1; fi
-if [[ -z "$roleTemplateId" ]]; then echo "Parameter is empty or missing: --roleTemplateId"; exit 1; fi
 
+# create or update identity and get service principal object id
+objectId=$(az identity create -g $resourcegroup -n $name --query principalId -o tsv | tr -d '\r')
+# get graph API service principal id
+graphSpId=$(az ad sp list --filter "DisplayName eq 'Microsoft Graph'" --query [0].objectId -o tsv | tr -d '\r')
+
+# Get role and assign to the managed identity service principal
+assign_app_role(){
+    appRoleId=$(az ad sp show --id $2 --query "appRoles[?value=='$3'].id" -o tsv | tr -d '\r')
+    if [[ -z "$appRoleId" ]]; then echo "App role '$3' does not exist in '$2'."; exit 1; fi
+    roleAssignmentId=$(az rest --method get --uri https://graph.microsoft.com/beta/servicePrincipals/$1/appRoleAssignments --query "value[?appRoleId=='$appRoleId'].id" -o tsv | tr -d '\r')
+    if [[ -z "$roleAssignmentId" ]]; then 
+        roleAssignmentId=$(az rest --method post --uri https://graph.microsoft.com/beta/servicePrincipals/$1/appRoleAssignments \
+            --body '{ "principalId": "'"$1"'", "resourceId": "'"$2"'", "appRoleId": "'"$appRoleId"'" }' --query id -o tsv | tr -d '\r')
+    fi
+    echo "App role '$3' ($appRoleId) assigned to '$1' as '$roleAssignmentId'"
+}
+
+assign_app_role $objectId $graphSpId Application.ReadWrite.All
+# ...
+
+
+#roleTemplateId=cf1c38e5-3621-4004-a7cb-879624dced7c # Application developer role
+#roleTemplateId=158c047a-c907-4556-b7ef-446551a6b5f7 # Cloud application admin role
+#if [[ -z "$roleTemplateId" ]]; then echo "Parameter is empty or missing: --roleTemplateId"; exit 1; fi
 # get access token for graph api
-accessToken=$(az account get-access-token --resource-type ms-graph --query "accessToken" -o tsv | tr -d '\r')
-# create or update identity
-objectId=$(az identity create -g $resourcegroup -n $name --query "principalId" -o tsv | tr -d '\r')
+#accessToken=$(az account get-access-token --resource-type ms-graph --query accessToken -o tsv | tr -d '\r')
 # delete existing identity member if it exists
-curl https://graph.microsoft.com/v1.0/directoryRoles/roleTemplateId=$roleTemplateId/members/$objectId/\$ref -X DELETE -H "Authorization: Bearer $accessToken"
+#curl https://graph.microsoft.com/v1.0/directoryRoles/roleTemplateId=$roleTemplateId/members/$objectId/\$ref -X DELETE -H "Authorization: Bearer $accessToken"
 # add identity as member of directory role
-curl https://graph.microsoft.com/v1.0/directoryRoles/roleTemplateId=$roleTemplateId/members/\$ref -X POST -d '{"@odata.id":"https://graph.microsoft.com/v1.0/directoryObjects/'"$objectId"'"}' -H "Content-Type: application/json" -H "Authorization: Bearer $accessToken"
+#curl https://graph.microsoft.com/v1.0/directoryRoles/roleTemplateId=$roleTemplateId/members/\$ref -X POST -d '{"@odata.id":"https://graph.microsoft.com/v1.0/directoryObjects/'"$objectId"'"}' -H "Content-Type: application/json" -H "Authorization: Bearer $accessToken"
+
 # return identity resource id to use in deployment
+
 az identity show -g $resourcegroup -n $name --query "id" -o tsv | tr -d '\r'
 
 
--- deploy/scripts/aad-register.sh
+++ deploy/scripts/aad-register.sh
@@ -5,31 +5,32 @@ usage(){
     exit 1
 }
 
+# Register service application in aad
+register_service(){
+    if [[ -z "$1" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
+    echo "Registering application '$1' in AAD tenant..."
+    objectId=$(az ad app create --display-name $1 --homepage "https://localhost" --query objectId -o tsv | tr -d '\r')
+    tenantName=$(az ad app show --id $objectId --query publisherDomain -o tsv | tr -d '\r')
+    az ad app update --id $objectId \
+        --identifier-uris https://$tenantName/$1 \
+        --set publicClient=false \
+        --set knownClientApplications='["04b07795-8ddb-461a-bbee-02f9e1bf7b46", "872cd9fa-d31f-45e0-9eab-6e460a02d1f1"]'
+    pw=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
+    az ad app credential reset --id $objectId --append --password $pw -o none
+    echo "Application '$objectId' registered in AAD tenant."
+    az ad app show --id $objectId \
+        --query "{serviceAppId:appId, servicePrincipalId:objectId, serviceAppSecret:'$pw', serviceAudience:identifierUris[0]}" -o json \
+        | tee $AZ_SCRIPTS_OUTPUT_PATH
+}
+
 # Unregister service application in aad
 unregister_service(){
     if [[ -z "$1" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
     echo "Unregistering application '$1' from AAD tenant..."
-    for objectId in $(az ad app list --display-name $applicationName --query [].objectId -o tsv | tr -d '\r'); do
+    for objectId in $(az ad app list --display-name $1 --query [].objectId -o tsv | tr -d '\r'); do
         az ad app delete --id $objectId
         echo "Application '$objectId' unregistered from AAD tenant."
     done
-}
-
-# Register service application in aad using application developer role permissions (can create, not update)
-register_service(){
-    if [[ -z "$1" ]] ; then echo "Parameter is empty or missing: --name"; usage; fi
-    echo "Registering application '$1' in AAD tenant..."
-    pw=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-    tenantName=$(az ad app list --all --query [0].publisherDomain -o tsv | tr -d '\r')
-    if [[ -z "$tenantName" ]] ; then
-        tenantName=$(az ad app create --display-name "__delete_me_$1" --query publisherDomain -o tsv | tr -d '\r')
-    fi
-    objectId=$(az ad app create --display-name $1 --password $pw --identifier-uris https://$tenantName/$1 --homepage "https://localhost" --query objectId -o tsv | tr -d '\r')
-    az ad app update --id $objectId --set knownClientApplications='["04b07795-8ddb-461a-bbee-02f9e1bf7b46", "872cd9fa-d31f-45e0-9eab-6e460a02d1f1"]'
-    az ad app update --id $objectId --set publicClient=false 
-    # az ad app credential reset --id $objectId --append --password $pw -o none
-    echo "Application '$objectId' registered in AAD tenant."
-    az ad app show --id $objectId --query "{serviceAppId:appId, servicePrincipalId:objectId, serviceAppSecret:'$pw', serviceAudience:identifierUris[0]}" -o json | tee $AZ_SCRIPTS_OUTPUT_PATH
 }
 
 if [[ -n "$AZ_SCRIPTS_OUTPUT_PATH" ]] ; then set -ex; az login --identity; fi
