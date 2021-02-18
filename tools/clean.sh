ad6fa7ba5d46fbe38bf492848b027b94a55d50f2
Marc Schier <mschier@microsoft.com>
2021-02-18 13:28:59 +0100

    update

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -ex
 
 for group in $(az group list --query "[?starts_with(name, 'testgroup')].name" -o tsv | tr -d '\r'); do
-    echo "deleting $group ..."
+    echo "deleting resourcegroup $group ..."
     az group delete -g $group -y
 done
+
+for vault in $(az keyvault list-deleted --query "[?contains(properties.vaultId, 'resourceGroups/testgroup')].name" -o tsv | tr -d '\r'); do
+    echo "deleting keyvault $vault ..."
+    az keyvault purge --name $vault
+done
+
