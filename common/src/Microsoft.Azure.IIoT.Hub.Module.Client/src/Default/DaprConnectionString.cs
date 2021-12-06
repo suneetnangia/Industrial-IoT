f578eb4dadbc9323b293c97b849a5e284b89f223
simonjaeger <simon.jager@live.com>
2021-12-06 18:25:17 +0100

    feat: add copyright header

--- common/src/Microsoft.Azure.IIoT.Hub.Module.Client/src/Default/DaprConnectionString.cs
+++ common/src/Microsoft.Azure.IIoT.Hub.Module.Client/src/Default/DaprConnectionString.cs
@@ -1,4 +1,9 @@
-﻿namespace Microsoft.Azure.IIoT.Module.Framework.Client {
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.IIoT.Module.Framework.Client {
     using System;
     using System.Collections.Generic;
     using System.Linq;
